8235a71ff40987262da363b4551a2542ecbf562c
Jakub Mir <xmir@node.mendelu.cz>
2025-12-22 17:22:48 +0100

    init

---
+++ CookieCrusher/CookieCrusher/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookieCrusher
+//
+//  Created by Jakub Mir on 22.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookieCrusher/CookieCrusher/App/CookieCrusherApp.swift
@@ -0,0 +1,30 @@
+//
+//  CookieCrusherApp.swift
+//  CookieCrusher
+//
+//  Created by Jakub Mir on 22.12.2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct CookieCrusherApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
